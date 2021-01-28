e5a8ebc60e5343fe83cc9e86e3ef87c417902cd1
Vijay Sadashivaiah <vijaysadashivaiah@gmail.com>
2021-01-28 13:13:51 -0500

    Update main_figure.m

--- main_figure.m
+++ main_figure.m
@@ -10,24 +10,30 @@ function main_figure(unmixed, M0, lambda, channel, filename)
 % Copyright (2020) Lieber Institute for Brain Development, Baltimore MD
 
 
-    [fPath, fName, ~] = fileparts(filename);
-    [~, maxdim] = max(M0);
-    maxL = lambda(maxdim);
-    map = struct;
-    unmix = struct;
-    colors = [];
-    for i=1:size(M0,2)-1
-        sRGB = spectrumRGB(maxL(i));
-        if contains(channel{i}, 'Lipo')
-            sRGB = [1,1,1];
-        end
-        unmix.(channel{i}) = squeeze(unmixed(:,:,i,:));
-        figure(i)
-        MIP = max(unmix.(channel{i})/max(unmix.(channel{i}),[],'all'), [], 3);
-        map.(channel{i}) = [linspace(0,sRGB(1),256)', linspace(0,sRGB(2),256)', linspace(0,sRGB(3),256)'];
-        imagesc(imadjust(MIP)); colormap(map.(channel{i}));
-        MIPTiff = ind2rgb(im2uint8(MIP), map.(channel{i}));
-        imgPath = fullfile(fPath,'Figures', [fName, '_MIP_', channel{i}, '.tif']);
-        imwrite(MIPTiff, imgPath);
+[fPath, fName, ~] = fileparts(filename);
+[~, maxdim] = max(M0);
+maxL = lambda(maxdim);
+map = struct;
+unmix = struct;
+colors = [];
+for i=1:size(M0,2)-1
+    sRGB = spectrumRGB(maxL(i));
+    if contains(channel{i}, 'Lipo')
+        sRGB = [1,1,1];
     end
+    unmix.(channel{i}) = squeeze(unmixed(:,:,i,:));
+    figure(i)
+    MIP = max(unmix.(channel{i})/max(unmix.(channel{i}),[],'all'), [], 3);
+    map.(channel{i}) = [linspace(0,sRGB(1),256)', linspace(0,sRGB(2),256)', linspace(0,sRGB(3),256)'];
+    imagesc(imadjust(MIP)); colormap(map.(channel{i}));
+    MIPTiff = ind2rgb(im2uint8(MIP), map.(channel{i}));
+    
+    % Output the image files
+    outdir = fullfile(fPath,'Output');
+    if ~exist(outdir, 'dir')
+        mkdir(outdir)
+    end
+    imgPath = fullfile(outdir, [fName, '_MIP_', channel{i}, '.tif']);
+    imwrite(MIPTiff, imgPath);
+end
 end
